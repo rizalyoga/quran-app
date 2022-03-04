60b2dfe20cae91f37a1e21828576f74f149bf3de
rizalyoga <ach.rizal.yogaswara@gmail.com>
2022-03-04 20:34:39 +0700

    add search bar

--- pages/quran/index.tsx
+++ pages/quran/index.tsx
@@ -1,6 +1,7 @@
 import Layout from "../../component/layout/layout";
 import style from "../../styles/Quran.module.css";
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 interface Post {
   id: string;
@@ -20,9 +21,23 @@ interface Id {
 }
 
 const Blog = (props: BlogProps) => {
+  const debounce = require("lodash.debounce");
+
   const { dataBlog } = props;
 
+  const [term, setTerm] = useState("");
+
   const router = useRouter();
+
+  const changeValue = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = await e.target.value;
+    setTerm(newValue);
+    console.log(term);
+  };
+
+  useEffect(() => {
+    console.log(term);
+  }, [term]);
 
   if (dataBlog.length === 0) {
     <Layout pageTitle="Surah">
@@ -33,15 +48,21 @@ const Blog = (props: BlogProps) => {
   return (
     <Layout pageTitle="Blog Page">
       <h1 className={style["title-page"]}>LIST OF SURAH</h1>
-      {dataBlog.map((blog) => (
-        <div key={blog.nama} className={style.container} onClick={() => router.push(`/quran/${blog.nomor}`)}>
-          <h4 className={style["title-surah"]}>{blog.nama}</h4>
-          <h4 className={style["title-page"]}>
-            Surah ke: {blog.nomor} | Jumlah ayat : {blog.ayat}
-          </h4>
-          <p className={style.content}>{blog.keterangan}</p>
-        </div>
-      ))}
+      <div className={style["search-form"]}>
+        <input type="search" onChange={changeValue} placeholder="Enter your keyword" />
+        {/* <p>search</p> */}
+      </div>
+      {dataBlog
+        .filter((surah) => surah.nama.toLowerCase().includes(term.toLowerCase()))
+        .map((blog) => (
+          <div key={blog.nama} className={style.container} onClick={() => router.push(`/quran/${blog.nomor}`)}>
+            <h4 className={style["title-surah"]}>{blog.nama}</h4>
+            <h4 className={style["title-page"]}>
+              Surah ke: {blog.nomor} | Jumlah ayat : {blog.ayat}
+            </h4>
+            <p className={style.content}>{blog.keterangan}</p>
+          </div>
+        ))}
     </Layout>
   );
 };
